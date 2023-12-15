49875ce4a5bdbbc6a71943c15eb1fb68db6d09f7
Hugo Simonfroy <hugo.simonfroy@gmail.com>
2023-12-15 11:42:45 +0100

    test lagrangian bias 1

--- montecosmo/models.py
+++ montecosmo/models.py
@@ -4,66 +4,75 @@ import jax.numpy as jnp
 import jax_cosmo as jc
 from jaxpm.kernels import fftk
 
-import jax
-
 
 
 def cosmo_prior(trace_reparam=False):
-  """
-  Defines a cosmological prior to sample from.
-  """
-  Omega_c_base = numpyro.sample('Omega_c_base', dist.TruncatedNormal(0,1, low=-1))
-  sigma8_base = numpyro.sample('sigma8_base', dist.Normal(0, 1))
-  Omega_c = Omega_c_base * 0.2 + 0.25
-  sigma8 = sigma8_base * 0.14 + 0.831
+    """
+    Defines a cosmological prior to sample from.
+    """
+    Omega_c_base = numpyro.sample('Omega_c_base', dist.TruncatedNormal(0,1, low=-1))
+    sigma8_base = numpyro.sample('sigma8_base', dist.Normal(0, 1))
+    Omega_c = Omega_c_base * 0.2 + 0.25
+    sigma8 = sigma8_base * 0.14 + 0.831
 
-  if trace_reparam:
-    Omega_c = numpyro.deterministic('Omega_c', Omega_c)
-    sigma8 = numpyro.deterministic('sigma8', sigma8)
+    if trace_reparam:
+        Omega_c = numpyro.deterministic('Omega_c', Omega_c)
+        sigma8 = numpyro.deterministic('sigma8', sigma8)
 
-  cosmo_params = {'Omega_c':Omega_c, 'sigma8':sigma8}
-  # numpyro.deterministic('cosmo_params', cosmo_params)
-  cosmology = jc.Planck15(**cosmo_params)
-  return cosmology
+    cosmo_params = {'Omega_c':Omega_c, 'sigma8':sigma8}
+    # numpyro.deterministic('cosmo_params', cosmo_params)
+    cosmology = jc.Planck15(**cosmo_params)
+    return cosmology
 
 # from numpyro.infer.reparam import LocScaleReparam
-#   reparam_config = {'Omega_c': LocScaleReparam(centered=0),
-#                     'sigma8': LocScaleReparam(centered=0)}
-#   with numpyro.handlers.reparam(config=reparam_config):
-#     Omega_c = numpyro.sample('Omega_c', dist.Normal(0.25, 0.2**2))
-#     sigma8 = numpyro.sample('sigma8', dist.Normal(0.831, 0.14**2))
+#     reparam_config = {'Omega_c': LocScaleReparam(centered=0),
+#                       'sigma8': LocScaleReparam(centered=0)}
+#     with numpyro.handlers.reparam(config=reparam_config):
+#         Omega_c = numpyro.sample('Omega_c', dist.Normal(0.25, 0.2**2))
+#         sigma8 = numpyro.sample('sigma8', dist.Normal(0.831, 0.14**2))
 
 
 def linear_field(mesh_size, box_size, pk, trace_reparam=False):
-  """
-  Generate initial conditions.
-  """
-  kvec = fftk(mesh_size)
-  kmesh = sum((kk / box_size[i] * mesh_size[i])**2 for i, kk in enumerate(kvec))**0.5
-  pkmesh = pk(kmesh) * (mesh_size.prod() / box_size.prod())
+    """
+    Generate initial conditions.
+    """
+    kvec = fftk(mesh_size)
+    kmesh = sum((kk  * (mesh_size[i] / box_size[i]))**2 for i, kk in enumerate(kvec))**0.5
+    pkmesh = pk(kmesh) * (mesh_size.prod() / box_size.prod())
 
-  field = numpyro.sample('init_mesh_base', dist.Normal(jnp.zeros(mesh_size), jnp.ones(mesh_size)))
-  jax.debug.print("{a}, {b}, {c}, {d}", a=field.shape, b=pkmesh.shape, c=jnp.array(kmesh).shape, d=(kvec[0].shape, kvec[1].shape, kvec[2].shape))
+    field = numpyro.sample('init_mesh_base', dist.Normal(jnp.zeros(mesh_size), jnp.ones(mesh_size)))
 
-  field = jnp.fft.rfftn(field) * pkmesh**0.5
-  jax.debug.print("FFT {a}, {b}", a=field.shape, b=pkmesh.shape)
-  field = jnp.fft.irfftn(field)
-  jax.debug.print("après iFFT {a}, {b}", a=field.shape, b=pkmesh.shape)
+    field = jnp.fft.rfftn(field) * pkmesh**0.5
+    field = jnp.fft.irfftn(field)
 
+    if trace_reparam:
+        field = numpyro.deterministic('init_mesh', field)
 
-  if trace_reparam:
-    field = numpyro.deterministic('init_mesh', field)
-
-  return field
+    return field
 
 
 def linear_pk_interp(cosmology, scale_factor=1, n_interp=256):
-  """
-  Return a light emulation of the linear matter power spectrum.
-  """
-  k = jnp.logspace(-4, 1, n_interp)
-  pk = jc.power.linear_matter_power(cosmology, k, a=scale_factor)
-  pk_fn = lambda x: jc.scipy.interpolate.interp(x.reshape([-1]), k, pk).reshape(x.shape)
-  return pk_fn
+    """
+    Return a light emulation of the linear matter power spectrum.
+    """
+    k = jnp.logspace(-4, 1, n_interp)
+    pk = jc.power.linear_matter_power(cosmology, k, a=scale_factor)
+    pk_fn = lambda x: jc.scipy.interpolate.interp(x.reshape([-1]), k, pk).reshape(x.shape)
+    return pk_fn
 
 
+def laplace_kernel(kvec):
+    """
+    Compute the Laplace kernel from a given K vector
+    Parameters:
+    -----------
+    kvec: array
+    Array of k values in Fourier space
+    Returns:
+    --------
+    wts: array
+    Complex kernel
+    """
+    kk = sum(ki**2 for ki in kvec)
+    kk[kk == 0] = jnp.inf # simpler version
+    return 1 / kk
--- montecosmo/utils.py
+++ montecosmo/utils.py
@@ -4,73 +4,79 @@ import pickle
 
 
 
-def sample_and_save(mcmc, model_kwargs, n_runs, save_path, save_var_names):
+def save_mcmc(mcmc, i_run, save_path, save_var_names, extra_fields):
     """
-    Warmup and run MCMC, and save the specified variables.
+    Save one run of MCMC sampling, with extra fields and last state.
+    """
+    samples = mcmc.get_samples()
+    samples = {key: samples[key] for key in save_var_names}
+
+    # Save warmup
+    with open(save_path+f"_{i_run}.p", 'wb') as file:
+        pickle.dump(samples, file, protocol=pickle.HIGHEST_PROTOCOL)
+    del samples
+
+    # Save extra fields
+    if extra_fields:
+        extra = mcmc.get_extra_fields()
+        with open(save_path+f"_extra_{i_run}.p", 'wb') as file:
+            pickle.dump(extra, file, protocol=pickle.HIGHEST_PROTOCOL)
+        del extra
+
+    # Save or overwrite last state
+    with open(save_path+f"_laststate.p", 'wb') as file:
+        pickle.dump(mcmc.last_state, file, protocol=pickle.HIGHEST_PROTOCOL)   
+
+
+def sample_and_save(mcmc, model_kwargs, n_runs, save_path, save_var_names, extra_fields:list=[], rng_key=random.PRNGKey(0)):
+    """
+    Warmup and run MCMC, saving the specified variables and extra fields.
     """
     # Warmup sampling
     if mcmc.num_warmup>=1:
         print(f"run {0}/{n_runs} (warmup)")
-        key_warmup = random.PRNGKey(0)
 
         # Warmup
-        mcmc.warmup(key_warmup, collect_warmup=True, **model_kwargs)
-        warmup_samples = mcmc.get_samples()
-        warmup_samples = {key: warmup_samples[key] for key in save_var_names}
-
-        # Saving warmup
-        with open(save_path+f"_{0}.p", 'wb') as file:
-            pickle.dump(warmup_samples, file, protocol=pickle.HIGHEST_PROTOCOL)
-        del warmup_samples
+        mcmc.warmup(rng_key, collect_warmup=True, **model_kwargs, extra_fields=extra_fields)
+        save_mcmc(mcmc, 0, save_path, save_var_names, extra_fields)
 
         # Handling rng key
         key_run = mcmc.post_warmup_state.rng_key
-
-        # Save or overwrite last state
-        with open(save_path+f"_laststate.p", 'wb') as file:
-            pickle.dump(mcmc.last_state, file, protocol=pickle.HIGHEST_PROTOCOL)
     else:
-        key_run = random.PRNGKey(0)
+        key_run = rng_key
 
     # Run sampling
     for i_run in range(1, n_runs+1):
         print(f"run {i_run}/{n_runs}")
             
         # Run
-        mcmc.run(key_run, **model_kwargs)
-        run_samples = mcmc.get_samples()
-        run_samples = {key: run_samples[key] for key in save_var_names}
-        
-        # Saving run
-        with open(save_path+f"_{i_run}.p", 'wb') as file:
-            pickle.dump(run_samples, file, protocol=pickle.HIGHEST_PROTOCOL)
-        del run_samples
+        mcmc.run(key_run, **model_kwargs, extra_fields=extra_fields)
+        save_mcmc(mcmc, i_run, save_path, save_var_names, extra_fields)
 
         # Init next run at last state
         mcmc.post_warmup_state = mcmc.last_state
         key_run = mcmc.post_warmup_state.rng_key
-
-        # Save or overwrite last state
-        with open(save_path+f"_laststate.p", 'wb') as file:
-            pickle.dump(mcmc.last_state, file, protocol=pickle.HIGHEST_PROTOCOL)
     return mcmc
 
 
-def load_runs(var_names, load_path, start_run, end_run):
+def load_runs(start_run, end_run, load_path, var_names=None):
     """
     Load and append runs saved in different files with same name.
+    If var_names is None, load all the variables.
     """
     samples = {}
     for i_run in range(start_run, end_run+1):
         # Load
         # post_samples_part = np.load(load_path+f"_{i_run}.npy", allow_pickle=True).item()
         with open(load_path+f"_{i_run}.p", 'rb') as file:
-          post_samples_part = pickle.load(file)
+          post_samples_part = pickle.load(file)        
 
         # Init or append samples
-        if not samples:
+        if samples == {}:
+            if var_names is None:
+                var_names = list(post_samples_part.keys())
             samples = {key: post_samples_part[key] for key in var_names}
         else:
             samples = {key: jnp.concatenate((samples[key], post_samples_part[key])) for key in var_names}
-    print(f"total num samples={samples[var_names[0]].shape[0]}")
+    print(f"total run length={samples[var_names[0]].shape[0]}")
     return samples
