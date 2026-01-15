fourier likelihood, naming convention, fNL sign fix

montecosmo/bricks.py
@@ -121,7 +121,7 @@ def trans_phi2delta_interp(cosmo:Cosmology, a=1., n_interp=256):
     z_norm = 10. # in matter-dominated era
     a_norm = 1. / (1. + z_norm)
     normalized_growth_factor = a2g(cosmo, a) / a2g(cosmo, a_norm) * a_norm
-    trans = - 2. * constants.rh**2 * ks**2 * trans_lin * normalized_growth_factor / (3. * cosmo.Omega)
+    trans = 2. * constants.rh**2 * ks**2 * trans_lin * normalized_growth_factor / (3. * cosmo.Omega)
     trans_fn = lambda x: jnp.interp(x.reshape(-1), ks, trans, left=0., right=0.).reshape(x.shape)
     return trans_fn
 
@@ -306,7 +306,7 @@ def lagrangian_bias(cosmo:Cosmology, pos, a, box_size, init_mesh,
 
     mesh_shape = delta.shape
     kvec = rfftk(mesh_shape)
-    kmesh = sum((ki  * (m / b))**2 for ki, m, b in zip(kvec, mesh_shape, box_size))**.5 # in h/Mpc 
+    kmesh = sum((ki  * (m / b))**2 for ki, m, b in zip(kvec, mesh_shape, box_size))**.5 # in h/Mpc
 
     # Init weights
     weights = 1.
@@ -345,6 +345,7 @@ def lagrangian_bias(cosmo:Cosmology, pos, a, box_size, init_mesh,
     # # Apply b3, punctual term
     # delta3_pos = delta_pos**3
     # weights += b3 * delta3_pos
+    # print("delta3_pos.mean()", delta3_pos.mean())
 
     if png_type is not None:
         if png_type == "fNL":
@@ -397,7 +398,8 @@ def b_phi(b1, p=1., delta_c=1.686):
     """
     # bp = 2 dc (b1 + 1 - p)
     # bE1 = 1 + b1
-    # bEp = bp = 2 dc (bE1 - p) 
+    # bEp = bp = 2 dc (bE1 - p)
+    # p=1 for halos, p=1+1/dc~1.6 for recent mergers, maybe p=0.55 for stellar mass selected samples.
     return 2 * delta_c * (b1 + 1 - p)
 
 def b_phi_delta(b1, b2, delta_c=1.686):

montecosmo/model.py
@@ -135,12 +135,12 @@ default_config={
                             'label':'{f}_\\mathrm{NL}',
                             'loc':0.,
                             'scale':1e3,
-                            'scale_fid':1e1,
+                            'scale_fid':1e2,
                             },
                 'fNL_bp': {'group':'png',
                             'label':'{f}_\\mathrm{NL} b_\\phi',
                             'loc':0.,
-                            'scale':1e3,
+                            'scale':3e3,
                             'scale_fid':1e1,
                             },
                 'fNL_bpd': {'group':'png',
@@ -174,8 +174,8 @@ default_config={
                                 'low':0.,
                                 'high':jnp.inf,
                                 },
-                'sigma_0': {'group':'syst',
-                                'label':'{\\sigma}_{0}',
+                's_0': {'group':'syst',
+                                'label':'{s}_{0}',
                                 'loc':1.,
                                 'scale':1.,
                                 # 'scale_fid':1e-1,
@@ -183,8 +183,24 @@ default_config={
                                 'low':0.,
                                 'high':jnp.inf,
                                 },
-                'sigma_delta': {'group':'syst',
-                                'label':'{\\sigma}_{\\delta}',
+                's_2': {'group':'syst',
+                                'label':'{s}_{2}',
+                                'loc':0.1,
+                                'scale':3e1,
+                                'scale_fid':1e0,
+                                # 'low':0.,
+                                # 'high':jnp.inf,
+                                },
+                's_mu2': {'group':'syst',
+                                'label':'{s}_{\\mu,2}',
+                                'loc':0.1,
+                                'scale':3e1,
+                                'scale_fid':1e0,
+                                # 'low':0.,
+                                # 'high':jnp.inf,
+                                },
+                's_delta': {'group':'syst',
+                                'label':'{s}_{\\delta}',
                                 'loc':1.,
                                 'scale':1.,
                                 'scale_fid':1e-1,
@@ -632,18 +648,21 @@ class FieldLevelModel(Model):
             _, _, _, a = tophysical_pos(pos, self.box_center, self.box_rot, self.box_size, self.evol_shape, 
                                     cosmology, self.a_obs, self.curved_sky)
 
+
             # Lagrangian bias expansion weights at a_obs (but based on initial particules positions)
             lbe_weights, dvel = lagrangian_bias(cosmology, pos, a, self.box_size, **init, **bias, **png, 
                                                 png_type=self.png_type, read_order=1)
-
+            
             if self.png_type is not None:
                 init['init_mesh'] = add_png(cosmology, png['fNL'], init['init_mesh'], self.box_size)
+            # init['init_mesh'] = chreshape(init['init_mesh'], r2chshape(self.init_shape))
+            # init['init_mesh'] = chreshape(init['init_mesh'], r2chshape(self.evol_shape))
 
             if self.evolution=='lpt':
                 # NOTE: lpt assumes given mesh is at a=1
                 cosmology._workspace = {} # HACK: force recompute by jaxpm cosmo to get g2, f2 => TODO: add g2, f2 to jaxcosmo
                 dpos, vel = lpt(cosmology, **init, pos=pos, a=a, lpt_order=self.lpt_order, 
-                                read_order=1, grad_fd=False, lap_fd=False)
+                                read_order=1, grad_fd=np.inf, lap_fd=np.inf)
                 pos += dpos
                 pos, vel = deterministic('lpt_ptcl', jnp.array((pos, vel)))
 
@@ -652,7 +671,7 @@ class FieldLevelModel(Model):
                 assert jnp.ndim(a) == 0, "N-body light-cone not implemented yet"
                 pos, vel = nbody_bf(cosmology, **init, pos=pos, a0=self.nbody_a_start, a1=a, n_steps=self.nbody_n_steps, 
                                     paint_order=self.paint_order, lpt_order=self.lpt_order,
-                                    grad_fd=False, lap_fd=False, snapshots=self.nbody_snapshots)
+                                    grad_fd=np.inf, lap_fd=np.inf, snapshots=self.nbody_snapshots)
                 pos, vel = deterministic('nbody_ptcl', jnp.array((pos, vel)))
                 pos, vel = tree.map(lambda x: x[-1], (pos, vel))
 
@@ -695,27 +714,6 @@ class FieldLevelModel(Model):
         """
         mesh, syst = params
 
-        # if self.observable == 'field':
-        #     mesh = mesh2masked(mesh * self.selec_mesh, self.mask)
-        #     mesh /= mesh.mean()
-
-        #     mesh -= 1
-        #     # print("mesh", mesh.mean(), mesh.std(), mesh.min(), mesh.max())
-        #     rcounts = syst['ngbars'] * self.cell_length**3
-        #     mean_count = rcounts.mean()
-
-        #     # if self.lik_type == 'gaussian':
-        #     #     # obs = sample('obs', dist.Normal((1 + mesh) * mean_count, mean_count**.5))
-        #     #     obs = sample('obs', dist.Normal(mesh, mean_count**-.5))
-
-        #     # else:
-        #     #     # posit_fn = lambda x: jnp.maximum(x, 1e-9)
-        #     #     # posit_fn = jnp.abs
-        #     #     posit_fn = lambda x: jnp.log(1 + jnp.exp(x))
-        #     #     obs = sample('obs', dist.Normal(mesh, (posit_fn(1 + syst['sigma_delta'] * mesh) / mean_count)**.5))
-        #     # return obs
-
-
         if self.observable == 'field':
             # print("mesh", mesh.mean(), mesh.std(), mesh.min(), mesh.max())
             mesh = mesh2masked(mesh * self.selec_mesh, self.mask)
@@ -736,15 +734,25 @@ class FieldLevelModel(Model):
                 obs = sample('obs', dist.Poisson(intens**(1 / temp)))
             else:
                 if self.lik_type == 'gaussian':
-                    var = syst['sigma_0']
+                    var = syst['s_0']
                 elif self.lik_type == 'gaussian_delta':
                     delta = mesh - 1
-                    var = posit_fn(1 + syst['sigma_delta'] * delta) * syst['sigma_0']
-                    # var = posit_fn((1 + syst['sigma_delta'] * delta) * syst['sigma_0'])
+                    var = posit_fn(1 + syst['s_delta'] * delta) * syst['s_0']
+                    # var = posit_fn((1 + syst['s_delta'] * delta) * syst['s_0'])
                 elif self.lik_type == 'gaussian_delta_power':
                     delta = mesh - 1
-                    var = (1 + delta)**2 * syst['sigma_delta'] + syst['sigma_0']
-                    # var = posit_fn((1 + syst['sigma_delta'] * delta) * syst['sigma_0'])
+                    var = jnp.abs(1 + delta)**1.7 * syst['s_delta'] + syst['s_0']
+                    # var = posit_fn((1 + syst['s_delta'] * delta) * syst['s_0'])
+                elif self.lik_type == 'gaussian_fourier':
+                    kvec = rfftk(self.final_shape)
+                    kmesh = sum((ki  * (m / b))**2 for ki, m, b in zip(kvec, self.final_shape, self.box_size))**.5 # in h/Mpc
+                    mumesh = sum(ki * losi * (m / b) for ki, losi, m, b in zip(kvec, self.los_fid, self.final_shape, self.box_size))
+                    mumesh = safe_div(mumesh, kmesh)
+
+                    # var = syst['s_0'] * (1 + syst['s_2'] * kmesh**2 + syst['s_mu2'] * (kmesh * mumesh)**2)
+                    var = syst['s_0'] * (1 + syst['s_2'] * kmesh**2 + syst['s_mu2'] * (kmesh * mumesh)**2)**2
+                    var = cgh2rg(var**.5, norm="amp")**2
+                    mesh = cgh2rg(jnp.fft.rfftn(mesh))
 
                 var *= mean_count
                 obs = sample('obs', dist.Normal(mesh * mean_count, (temp * var)**.5))

montecosmo/nbody.py
@@ -40,7 +40,7 @@ def fftk(shape):
     return kx, ky, kz
 
 
-def invlaplace_hat(kvec, fd=False):
+def invlaplace_hat(kvec, fd_order=np.inf):
     """
     Fourier transform of inverse Laplace kernel.
 
@@ -48,22 +48,24 @@ def invlaplace_hat(kvec, fd=False):
     -----------
     kvec: list
         List of wavevectors
-    fd: bool
-        Finite difference kernel
+    fd_order: int
+        Finite difference order
 
     Returns
     --------
     weights: array
         Complex kernel values
     """
-    if fd:
-        kk = sum((ki * np.sinc(ki / (2 * np.pi)))**2 for ki in kvec)
-    else:
+    if fd_order == 2:
+        kk = sum((np.cos(ki) - 1) * 2 for ki in kvec)
+    elif fd_order == 4:
+        kk = sum((np.cos(2 * ki) - 16 * np.cos(ki) + 15) / 6 for ki in kvec)
+    elif fd_order == np.inf:
         kk = sum(ki**2 for ki in kvec)
     return - safe_div(1, kk)
 
 
-def gradient_hat(kvec, direction:int, fd=False):
+def gradient_hat(kvec, direction:int, fd_order=np.inf):
     """
     Fourier transform of gradient kernel along given direction.
     
@@ -73,8 +75,8 @@ def gradient_hat(kvec, direction:int, fd=False):
         List of wavevectors
     direction: int
         Index of the direction in which to take the gradient
-    fd: bool
-        Finite difference kernel
+    fd_order: int
+        Finite difference order
 
     Returns
     --------
@@ -82,8 +84,12 @@ def gradient_hat(kvec, direction:int, fd=False):
         Complex kernel values
     """
     ki = kvec[direction]
-    if fd:
-        ki = (8. * np.sin(ki) - np.sin(2. * ki)) / 6.
+    if fd_order == 2:
+        ki = np.sin(ki)
+    elif fd_order == 4:
+        ki = (8 * np.sin(ki) - np.sin(2 * ki)) / 6
+    elif fd_order == np.inf:
+        pass
     return 1j * ki
 
 
@@ -455,7 +461,7 @@ def interlace(pos, shape:tuple, weights=1., paint_order:int=2, interlace_order:i
 # Forces #
 ##########
 def pm_forces(pos, mesh:tuple|jnp.ndarray, read_order:int=2, 
-              paint_deconv:bool=False, grad_fd=False, lap_fd=False, kcut=np.inf):
+              paint_deconv:bool=False, grad_fd=np.inf, lap_fd=np.inf, kcut=np.inf):
     """
     Compute gravitational forces on particles using a PM scheme
     """
@@ -476,7 +482,7 @@ def pm_forces(pos, mesh:tuple|jnp.ndarray, read_order:int=2,
                       for i in range(3)], axis=-1)
 
 
-def pm_forces2(pos, mesh:jnp.ndarray, read_order:int=2, lap_fd=False, grad_fd=False):
+def pm_forces2(pos, mesh:jnp.ndarray, read_order:int=2, lap_fd=np.inf, grad_fd=np.inf):
     """
     Return 2LPT source term.
     """
@@ -508,8 +514,8 @@ def lpt(
     a: float| jnp.ndarray,
     lpt_order: int = 2,
     read_order: int = 2,
-    grad_fd: bool = False,
-    lap_fd: bool = False,
+    grad_fd: int = np.inf,
+    lap_fd: int = np.inf,
     ) -> tuple[jnp.ndarray, jnp.ndarray]:
     """
     Compute first or second order LPT displacement, at given scale factor(s).
@@ -759,7 +765,7 @@ def chi2a(cosmo, chi, log10_amin=dist_log10_amin, steps=dist_steps):
 # Solvers #
 ###########
 def bullfrog_vf(cosmo:Cosmology, dg, mesh_shape:tuple, paint_order:int=2, 
-                paint_deconv=False, grad_fd=False, lap_fd=False):
+                paint_deconv=False, grad_fd=np.inf, lap_fd=np.inf):
     """
     BullFrog vector field.
     """
@@ -824,7 +830,7 @@ def save_y(t, y, args):
     return y
 
 def nbody_bf(cosmo:Cosmology, init_mesh, pos, a0=0., a1=1., n_steps=5, 
-             paint_order:int=2, lpt_order:int=2, grad_fd=False, lap_fd=False, 
+             paint_order:int=2, lpt_order:int=2, grad_fd=np.inf, lap_fd=np.inf, 
              snapshots:int|list=None, fn=save_y):
     """
     N-body simulation with BullFrog solver.
@@ -862,7 +868,7 @@ def nbody_bf(cosmo:Cosmology, init_mesh, pos, a0=0., a1=1., n_steps=5,
 
 
 def nbody_bf_scan(cosmo:Cosmology, init_mesh, pos, a, n_steps=5, paint_order:int=2,
-              grad_fd=False, lap_fd=False, snapshots:int|list=None):
+              grad_fd=np.inf, lap_fd=np.inf, snapshots:int|list=None):
     """
     No-diffrax version of N-body simulation with BullFrog solver. 
     Simpler but does not optimize for memory usage with binomial checkpointing.
@@ -905,7 +911,7 @@ def nbody_bf_scan(cosmo:Cosmology, init_mesh, pos, a, n_steps=5, paint_order:int
 
 
 
-# def lpt_fpm(cosmo:Cosmology, init_mesh, pos, a, lpt_order:int=1, paint_order:int=2, grad_fd=True, lap_fd=False):
+# def lpt_fpm(cosmo:Cosmology, init_mesh, pos, a, lpt_order:int=1, paint_order:int=2, grad_fd=True, lap_fd=np.inf):
 #     """
 #     Computes first and second order LPT displacement, e.g. Eq. 2 and 3 [Jenkins2010](https://arxiv.org/pdf/0910.0258)
 #     """
@@ -950,7 +956,7 @@ def nbody_bf_scan(cosmo:Cosmology, init_mesh, pos, a, n_steps=5, paint_order:int
 #     return dq, p
 
 
-# def diffrax_vf(cosmo:Cosmology, mesh_shape, paint_order, grad_fd=True, lap_fd=False):
+# def diffrax_vf(cosmo:Cosmology, mesh_shape, paint_order, grad_fd=True, lap_fd=np.inf):
 #     """
 #     N-body ODE vector field for diffrax, e.g. Tsit5 or Dopri5
 
@@ -968,7 +974,7 @@ def nbody_bf_scan(cosmo:Cosmology, init_mesh, pos, a, n_steps=5, paint_order:int
 #     return vector_field
 
 
-# def jax_ode_vf(cosmo:Cosmology, mesh_shape, paint_order, grad_fd=True, lap_fd=False):
+# def jax_ode_vf(cosmo:Cosmology, mesh_shape, paint_order, grad_fd=True, lap_fd=np.inf):
 #     """
 #     Return N-body ODE vector field for jax.experimental.ode.odeint
 
@@ -983,7 +989,7 @@ def nbody_bf_scan(cosmo:Cosmology, init_mesh, pos, a, n_steps=5, paint_order:int
 
 # from diffrax import diffeqsolve, ODETerm, SaveAt, Euler, Heun, Dopri5, Tsit5, PIDController, ConstantStepSize
 # def nbody_tsit5(cosmo:Cosmology, mesh_shape, particles, a_lpt, a_obs, tol=1e-2, 
-#                 paint_order:int=2, grad_fd=True, lap_fd=False, snapshots:int|list=None):
+#                 paint_order:int=2, grad_fd=True, lap_fd=np.inf, snapshots:int|list=None):
 #     if a_lpt == a_obs:
 #         return tree.map(lambda x: x[None], particles)
 #     else:
@@ -1008,7 +1014,7 @@ def nbody_bf_scan(cosmo:Cosmology, init_mesh, pos, a, n_steps=5, paint_order:int
 
 # from montecosmo.fpm import EfficientLeapFrog, LeapFrogODETerm, symplectic_ode
 # def nbody_fpm(cosmo:Cosmology, mesh_shape, particles, a_lpt, a_obs, n_steps=5, 
-#               paint_order:int=2, grad_fd=True, lap_fd=False, snapshots=None):
+#               paint_order:int=2, grad_fd=True, lap_fd=np.inf, snapshots=None):
 #     if a_lpt == a_obs:
 #         return tree.map(lambda x: x[None], particles)
 #     else:

montecosmo/script.py
@@ -309,12 +309,13 @@ def compare_chains(load_dirs, labels, save_dir="./"):
         print('\n', chains.shape)
         gdsamp = chains.prune()[list(model.groups)+['~init_mesh']].to_getdist(label)
         # gdsamp = chains.prune()[['bias','png']+['~init_mesh']].to_getdist(label)
+        # gdsamp = chains.prune()[['bias','fNL_bp']+['~init_mesh']].to_getdist(label)
         # gdsamp = chains.prune()[['b1','png']+['~init_mesh']].to_getdist(label)
         # gdsamp = chains.prune()[['b1','fNL_bp']+['~init_mesh']].to_getdist(label)
         chainss.append(chains)
         gdsamps.append(gdsamp)
 
-        
+
     # chains['fNL_bp'] = chains['fNL'] * b_phi( chains['b1'], p=1.)
     # chains['fNL_bpd'] = chains['fNL'] * b_phi_delta( chains['b1'], chains['b2'])
 
@@ -324,31 +325,31 @@ def compare_chains(load_dirs, labels, save_dir="./"):
                     title_limit=1,
                     filled=True, 
                     # markers=truth,
-                    markers={k:v for k,v in truth.items() if k in ['fNL', 'fNL_bp', 'fNL_bpd']},
+                    # markers={k:v for k,v in truth.items() if k in ['fNL', 'fNL_bp', 'fNL_bpd']},
                     contour_colors=[SetDark2(i) for i in range(len(gdsamps))],)
     plt.savefig(save_dir / f"triangle_{'_'.join(labels)[:200]}.png", dpi=300)
 
 
 
-    mesh_ref = truth['init_mesh']
-    kpow_ref = model.spectrum(mesh_ref)
-    plt.figure(figsize=(12, 4), layout='constrained')
-    def plot_kptcs(kptcs, label=None, i_color=0):
-        color = SetDark2(i_color)
-        plot_powtranscoh(*kptcs, fill=0.68, color=color)
-        plot_powtranscoh(*kptcs, fill=0.95, color=color)
-        plot_powtranscoh(*tree.map(lambda x: jnp.median(x, 0), kptcs), color=color, label=label)
+    # mesh_ref = truth['init_mesh']
+    # kpow_ref = model.spectrum(mesh_ref)
+    # plt.figure(figsize=(12, 4), layout='constrained')
+    # def plot_kptcs(kptcs, label=None, i_color=0):
+    #     color = SetDark2(i_color)
+    #     plot_powtranscoh(*kptcs, fill=0.68, color=color)
+    #     plot_powtranscoh(*kptcs, fill=0.95, color=color)
+    #     plot_powtranscoh(*tree.map(lambda x: jnp.median(x, 0), kptcs), color=color, label=label)
 
-    plt.subplot(131)
-    plot_pow(*kpow_ref, 'k:', label='true')
-    plt.subplot(132)
-    plt.axhline(1., linestyle=':', color='k', alpha=0.5)
-    plt.subplot(133)
-    plt.axhline(model.selec_mesh.mean(), linestyle=':', color='k', alpha=0.5)
+    # plt.subplot(131)
+    # plot_pow(*kpow_ref, 'k:', label='true')
+    # plt.subplot(132)
+    # plt.axhline(1., linestyle=':', color='k', alpha=0.5)
+    # plt.subplot(133)
+    # plt.axhline(model.selec_mesh.mean(), linestyle=':', color='k', alpha=0.5)
 
-    for i, (chains, label) in enumerate(zip(chainss, labels)):
-        kptcs = tree.map(jnp.concatenate, chains['kptc'])
-        plot_kptcs(kptcs, label=label, i_color=i)
-    plt.subplot(131)
-    plt.legend()
-    plt.savefig(save_dir / f"kptc_{'_'.join(labels)[:200]}.png", dpi=300)
+    # for i, (chains, label) in enumerate(zip(chainss, labels)):
+    #     kptcs = tree.map(jnp.concatenate, chains['kptc'])
+    #     plot_kptcs(kptcs, label=label, i_color=i)
+    # plt.subplot(131)
+    # plt.legend()
+    # plt.savefig(save_dir / f"kptc_{'_'.join(labels)[:200]}.png", dpi=300)

montecosmo/script_pipe_fpm.py
@@ -44,19 +44,19 @@ def infer_model(mesh_length, eh_approx=True, oversamp=0, s8=False, overselect=No
     from datetime import datetime
     import os; os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION']='1.' # NOTE: jax preallocates GPU (default 75%)
     
-    # save_dir = Path(os.path.expanduser("~/scratch/png/abacus_c0_i0_z08_lrg")) # FMN
+    # save_dir = Path(os.path.expanduser("~/scratch/png/abacus_c0_i0_z0.8_lrg")) # FMN
     # save_dir = Path("/lustre/fsn1/projects/rech/fvg/uvs19wt/png/") # JZ
     # save_dir = Path("/lustre/fswork/projects/rech/fvg/uvs19wt/workspace/png/") # JZ
-    # main_dir = Path("/pscratch/sd/h/hsimfroy/png/abacus_c0_i0_z08_lrg") # Perlmutter
-    main_dir = Path("/pscratch/sd/h/hsimfroy/png/fpm_b2760_z05_lrg_fNL") # Perlmutter
+    # main_dir = Path("/pscratch/sd/h/hsimfroy/png/abacus_c0_i0_z0.8_lrg") # Perlmutter
+    main_dir = Path("/pscratch/sd/h/hsimfroy/png/fpm_b2760_z1_lrg_fNL") # Perlmutter
     load_dir = main_dir / "load"
 
-    fNL_true = 0
+    fNL_true = 100
 
     # save_dir = main_dir / f"tracer_real_eh{eh_approx:d}_ovsamp{oversamp:d}_s8{s8:d}_fNL"
     save_dir = main_dir / (f"tracer_fpmred_eh{eh_approx:d}_ovsamp{oversamp:d}_s8{s8:d}" + ("_fNL" if png_type=='fNL' else "_fNLb" if png_type=='fNL_bias' else ""))
     # save_dir = main_dir / f"selfspec_red_eh{eh_approx:d}_ovsamp{oversamp:d}_s8{s8:d}_fNL"
-    save_dir /= f"lpt_{mesh_length:d}" + (f"_osel{overselect}" if overselect is not None else "") + f"_fNL{fNL_true:.0f}" 
+    save_dir /= f"lpt_{mesh_length:d}" + (f"_osel{overselect}" if overselect is not None else "") + f"_fNL{fNL_true:.0f}" + "_likfourier_order1"
 
     chains_dir = save_dir / "chains"
     chains_dir.mkdir(parents=True, exist_ok=True)
@@ -80,7 +80,7 @@ def infer_model(mesh_length, eh_approx=True, oversamp=0, s8=False, overselect=No
     jconfig.update("jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir")
 
     from montecosmo.model import FieldLevelModel, default_config
-    from montecosmo.utils import pdump, pload, chreshape, r2chshape, boxreshape
+    from montecosmo.utils import pdump, pload, chreshape, r2chshape, boxreshape, cgh2rg
     from montecosmo.bricks import top_hat_selection, gen_gauss_selection
     
 
@@ -139,7 +139,8 @@ def infer_model(mesh_length, eh_approx=True, oversamp=0, s8=False, overselect=No
                             'k_cut': np.inf,
                             'init_power': load_dir / f'init_kpow.npy' if not eh_approx else None,
                             # 'init_power': None,
-                            'lik_type': 'gaussian_delta',
+                            # 'lik_type': 'gaussian_delta',
+                            'lik_type': 'gaussian_fourier',
                             'png_type': png_type,
                             # 'precond': 'kaiser_dyn'
                             } | oversamp_config)
@@ -150,13 +151,13 @@ def infer_model(mesh_length, eh_approx=True, oversamp=0, s8=False, overselect=No
         # 'b1': 0.,
         # 'b2': 0.,
         # 'bs2': 0.,
-        'b1': 1.15,
+        'b1': 0.7,
         'b2': 0.,
         'bs2': 0.,
         'bn2': 0.,
         'bnpar': 0.,
         'fNL': 0.,
-        'fNL_bp':0.,
+        'fNL_bp':25.,
         'fNL_bpd':0.,
         'alpha_iso': 1.,
         'alpha_ap': 1.,
@@ -164,8 +165,33 @@ def infer_model(mesh_length, eh_approx=True, oversamp=0, s8=False, overselect=No
         'ngbars': 1e-4,
         # 'ngbars': 10000., # neglect lik noise
         'sigma_0': 0.3,
+        'sigma_2': 1e-3,
+        'sigma_mu2': 1e-3,
         'sigma_delta': 0.7,
         }
+    
+    # truth = {
+    #     'Omega_m': 0.3137721, 
+    #     'sigma8': 0.8076353990239834,
+    #     'b1': 1.0,
+    #     'b2': 0.32,
+    #     'bs2': -0.71,
+    #     'bn2': -41.,
+    #     'bnpar': -22.,
+    #     'fNL': 0.,
+    #     'fNL_bp': 0.,
+    #     'fNL_bpd': 0.,
+    #     'alpha_iso': 1.,
+    #     'alpha_ap': 1.,
+    #     # 'ngbars': 8.43318125e-4,
+    #     # 'ngbars': 1e-4,
+    #     'ngbars': 1e-5,
+    #     # 'ngbars': 10000., # neglect lik noise
+    #     'sigma_0': 0.23,
+    #     'sigma_2': 1e-3,
+    #     'sigma_mu2': 1e-3,
+    #     'sigma_delta': 0.35,
+    # }
 
     latents = model.new_latents_from_loc(truth, update_prior=True)
     model = FieldLevelModel(**model.asdict() | {'latents': latents})
@@ -194,6 +220,7 @@ def infer_model(mesh_length, eh_approx=True, oversamp=0, s8=False, overselect=No
     elif fNL_true == -100:
         obs_mesh = jnp.load(load_dir / f'tracer_2099359_fNL-100_paint2_deconv1_{mesh_length}.npy')
     obs_mesh *= truth['ngbars'] * model.cell_length**3
+    
 
     # # Abacus initial
     init_mesh = jnp.fft.rfftn(jnp.load(load_dir / f"init_mesh_fake_2760_{256}.npy"))
@@ -238,6 +265,7 @@ def infer_model(mesh_length, eh_approx=True, oversamp=0, s8=False, overselect=No
 
     model.save(save_dir / "model.yaml")    
     jnp.savez(save_dir / "truth.npz", **truth)
+    sys.stdout.flush()
 
 
 
@@ -251,8 +279,10 @@ def infer_model(mesh_length, eh_approx=True, oversamp=0, s8=False, overselect=No
     tune_mass = True
 
     model.reset()
-    model.substitute({'obs': truth['obs']} | model.loc_fid, from_base=True)
-    print(model.data.keys()) ########
+    # model.substitute({'obs': truth['obs']} | model.loc_fid, from_base=True)
+    model.substitute({'obs': cgh2rg(jnp.fft.rfftn(truth['obs']))} | model.loc_fid, from_base=True) ###XXX
+
+    print('data params:', model.data.keys())
     model.block()
     params_start = jit(vmap(partial(model.kaiser_post, delta_obs=model.count2delta(truth['obs']), scale_field=2/3)))(jr.split(jr.key(45), n_chains))
     print('start params:', params_start.keys())
@@ -306,8 +336,8 @@ def infer_model(mesh_length, eh_approx=True, oversamp=0, s8=False, overselect=No
     plot_trans(kpow_true[0], (kpow_fid[1] / kpow_true[1])**.5, 'k--', alpha=0.5, label='fiducial')
     plt.subplot(133)
     plt.axhline(model.selec_mesh.mean(), linestyle=':', color='k', alpha=0.5)
-    plt.savefig(save_dir / 'init_warm.png')   
-
+    plt.savefig(save_dir / 'init_warm.png')
+    sys.stdout.flush()
 
 
 
@@ -328,10 +358,12 @@ def infer_model(mesh_length, eh_approx=True, oversamp=0, s8=False, overselect=No
         # 'fNL_bp','fNL_bpd',
         #    'bnpar',
             # 'b1',
-            # 'b2','bs2','bn2', 
+            'b2','bs2','bn2', 
             # 'ngbars', 
             # 'sigma_0',
-            # 'sigma_delta', 
+            'sigma_2',
+            'sigma_mu2',
+            'sigma_delta',
             # 'Omega_m',
             # 'sigma8',
             # 'init_mesh',
@@ -340,7 +372,10 @@ def infer_model(mesh_length, eh_approx=True, oversamp=0, s8=False, overselect=No
     obs += ['sigma8'] if not s8 else []
     obs += ['fNL_bp','fNL_bpd'] if not png_type=='fNL_bias' else []
     obs += ['fNL'] if not ('fNL'==png_type or 'fNL_bias'==png_type) else []
+    
+    # obs += ['fNL']
     obs = {k: truth[k] for k in obs}
+    obs |= {'obs': cgh2rg(jnp.fft.rfftn(truth['obs']))} ###XXX
 
     model.reset()
     model.substitute(obs, from_base=True)
@@ -402,6 +437,7 @@ def infer_model(mesh_length, eh_approx=True, oversamp=0, s8=False, overselect=No
         while os.path.exists(chains_dir / f"run_{start}.npz") and start <= n_runs:
             start += 1
         print(f"Resuming at run {start}...")
+    sys.stdout.flush()
 
     print("\nRunning...")
     run_fn = jit(vmap(get_mclmc_run(model.logpdf, n_samples, thinning=64, progress_bar=False)))
@@ -488,25 +524,26 @@ if __name__ == '__main__':
     png_types = ['fNL_bias']  # 'fNL', 'fNL_bias', None
     # infer_model = tm.python_app(infer_model)
     
-    # for mesh_length in mesh_lengths:
-    #     for eh_approx in eh_approxs:
-    #         for oversamp in oversamps:
-    #             for s8 in s8s:
-    #                 for overselect in overselects:
-    #                     for png_type in png_types:
-    #                         print(f"\n=== mesh_length: {mesh_length}, eh_approx: {eh_approx}, oversamp: {oversamp}, s8: {s8}, oversel: {overselect}, png_type: {png_type} ===")
-    #                         infer_model(mesh_length, eh_approx=eh_approx, oversamp=oversamp, s8=s8, 
-    #                                     overselect=overselect, png_type=png_type)
+    for mesh_length in mesh_lengths:
+        for eh_approx in eh_approxs:
+            for oversamp in oversamps:
+                for s8 in s8s:
+                    for overselect in overselects:
+                        for png_type in png_types:
+                            print(f"\n=== mesh_length: {mesh_length}, eh_approx: {eh_approx}, oversamp: {oversamp}, s8: {s8}, oversel: {overselect}, png_type: {png_type} ===")
+                            infer_model(mesh_length, eh_approx=eh_approx, oversamp=oversamp, s8=s8, 
+                                        overselect=overselect, png_type=png_type)
 
     # # # overwrite = False
     # overwrite = True
     # save_dir = "/pscratch/sd/h/hsimfroy/png/fpm_b2760_z05_lrg_fNL/tracer_fpmred_eh0_ovsamp2_s80_fNLb/lpt_64_fNL-100"
     # make_chains_dir(save_dir, start=1, end=100, thinning=1, reparb=False, overwrite=overwrite)
 
-    save_dir = "/pscratch/sd/h/hsimfroy/png/fpm_b2760_z05_lrg_fNL/tracer_fpmred_eh0_ovsamp2_s80_fNLb"
-    compare_chains_dir(save_dir,
-                       labels=["$f_\\mathrm{NL}=-100$","$f_\\mathrm{NL}=0$", "$f_\\mathrm{NL}=100$"],
-                       names=["lpt_64_fNL-100", "lpt_64_fNL0", "lpt_64_fNL100"])
+    # save_dir = "/pscratch/sd/h/hsimfroy/png/fpm_b2760_z05_lrg_fNL/tracer_fpmred_eh0_ovsamp2_s80_fNLb"
+    # compare_chains_dir(save_dir,
+    #                    labels=["$f_\\mathrm{NL}=100$","$f_\\mathrm{NL}=0$", "$f_\\mathrm{NL}=-100$"],
+    #                    names=["lpt_64_fNL-100", "lpt_64_fNL0", "lpt_64_fNL100"])
+    #                 #    names=["lpt_32_fNL-100", "lpt_32_fNL0", "lpt_32_fNL100"])
 
     # spawn(queue, spawn=True)
     print("Kenavo")

montecosmo/utils.py
@@ -500,13 +500,15 @@ def id_cgh(shape, part="real", norm="backward"):
     hx, hy, hz = shape//2
     chshape = (sx, sy, hz+1)
     
-    weights = np.ones(chshape) / 2**.5
+    weights = np.ones(chshape)
     if norm == "backward":
-        weights *= shape.prod()**.5 
+        weights /= (2 / shape.prod())**.5
+    elif norm == "ortho":
+        weights /= 2**.5
     elif norm == "forward":
-        weights /= shape.prod()**.5
+        weights /= (2 * shape.prod())**.5
     else:
-        assert norm=="ortho", "norm must be either 'backward', 'forward', or 'ortho'."
+        assert norm == "amp", "norm must be either 'backward', 'forward', 'ortho', or 'amp'."
 
     dtype = 'int16' # int16 -> +/- 32_767, trkl
     id = np.zeros((3, *chshape), dtype=dtype)
@@ -543,7 +545,7 @@ def id_cgh(shape, part="real", norm="backward"):
 
 
 
-def rg2cgh2(mesh, amp:bool=False, norm="backward"):
+def rg2cgh2(mesh, norm="backward"):
     """
     Permute a real Gaussian tensor (3D) into a complex Gaussian Hermitian tensor.
     particular `rg2cgh(N(0,I), norm)` is distributed as `rfftn(N(0,I), norm)`
@@ -551,14 +553,15 @@ def rg2cgh2(mesh, amp:bool=False, norm="backward"):
     shape = mesh.shape
     id_real, w_real = id_cgh(shape, part="real", norm=norm)
     id_imag, w_imag = id_cgh(shape, part="imag", norm=norm)
-    if not amp:
-        return mesh[id_real] * w_real + 1j * mesh[id_imag] * w_imag
-    else:
+    
+    if norm == "amp":
         # Average wavevector real and imaginary power and return amplitude
         return ((mesh[id_real]**2 + mesh[id_imag]**2) / 2)**.5
+    else:
+        return mesh[id_real] * w_real + 1j * mesh[id_imag] * w_imag
 
 
-def cgh2rg2(meshk, amp:bool=False, norm="backward"):
+def cgh2rg2(meshk, norm="backward"):
     """
     Permute a complex Gaussian Hermitian tensor into a real Gaussian tensor (3D).
     In particular `rg2cgh(N(0,I), norm)` is distributed as `rfftn(N(0,I), norm)`
@@ -568,15 +571,15 @@ def cgh2rg2(meshk, amp:bool=False, norm="backward"):
     id_imag, w_imag = id_cgh(shape, part="imag", norm=norm)
     
     mesh = jnp.zeros(shape)
-    if not amp:
+    if norm == "amp":
+        # Give same amplitude to wavevector real and imaginary part
+        mesh = mesh.at[id_imag].set(meshk.real)
+        mesh = mesh.at[id_real].set(meshk.real)
+    else:
         # NOTE: w_imag can be zero, which is not safe for gradients
         mesh = mesh.at[id_imag].set(safe_div(meshk.imag, w_imag)) 
         mesh = mesh.at[id_real].set(meshk.real / w_real)
         # NOTE: real after imag to overwrite the 2^3=8 points
-    else:
-        # Give same amplitude to wavevector real and imaginary part
-        mesh = mesh.at[id_imag].set(meshk.real)
-        mesh = mesh.at[id_real].set(meshk.real)
     return mesh
 
 